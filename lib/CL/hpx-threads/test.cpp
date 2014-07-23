7063e9aa883212d5ca59afeab7054a0d1a34a033
Finomnis <martin.h.stumpf@gmail.com>
2014-07-22 20:56:13 -0500

    so far, finished configure.ac

---
+++ lib/CL/hpx-threads/test.cpp
@@ -0,0 +1,9 @@
+#include<hpx/hpx_init.hpp>
+              int hpx_main(int argc, char* argv[])
+              {
+                  return hpx::finalize();
+              }
+              int main(int argc, char* argv[])
+              {
+                  return hpx::init(argc, argv);
+              }
